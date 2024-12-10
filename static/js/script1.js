ee0379c5f45c6523ef9808e1e7f031f449715300
Het Jain <113243568+het6620@users.noreply.github.com>
2024-12-10 07:06:08 +0530

    Faq

--- static/js/script1.js
+++ static/js/script1.js
@@ -1,5 +1,27 @@
-(function(){
-    "use strict";
-    new PureCounter();
+// (function(){
+//     "use strict";
+//     new PureCounter();
 
+
+    
+//   /**
+//    * Frequently Asked Questions Toggle
+//    */
+//   document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+//     faqItem.addEventListener('click', () => {
+//       faqItem.parentNode.classList.toggle('faq-active');
+//     });
+//   });
+
+//   console.log("Hello");
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+        faqItem.addEventListener('click', () => {
+            faqItem.parentNode.classList.toggle('faq-active');
+        });
+    });
+
+    console.log("hello");
 });
